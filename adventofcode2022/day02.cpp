5ddf111396c7c66a57d631dbc1e62c5d7d4e6a0c
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2022-12-02 08:54:04 +0100

    AOC 2022, Day 2: Rock Paper Scissors

--- adventofcode2022/day02.cpp
+++ adventofcode2022/day02.cpp
@@ -1,12 +1,80 @@
 #include <iostream>
-
 #include "utils.h"
+
+int calculate_score(char opponent_move, char next_move)
+{
+    int round_score{0};
+    switch (next_move)
+    {
+    case 'A':
+        if (opponent_move == 'C')
+            round_score = 6;
+        else if (opponent_move == 'A')
+            round_score = 3;
+        break;
+    case 'B':
+        if (opponent_move == 'A')
+            round_score = 6;
+        else if (opponent_move == 'B')
+            round_score = 3;
+        break;
+    case 'C':
+        if (opponent_move == 'B')
+            round_score = 6;
+        else if (opponent_move == 'C')
+            round_score = 3;
+        break;
+    }
+
+    int shape_score = next_move - 'A' + 1;
+
+    return shape_score + round_score;
+}
+
+char calculate_move(char opponent_move, char round_should_end)
+{
+    char next_move{};
+    switch (round_should_end)
+    {
+    case 'X':
+        next_move = opponent_move - 1;
+        break;
+    case 'Y':
+        next_move = opponent_move;
+        break;
+    case 'Z':
+        next_move = opponent_move + 1;
+        break;
+    }
+
+    if (next_move < 'A') next_move = 'C';
+    else if (next_move > 'C') next_move = 'A';
+
+    return next_move;
+}
 
 int main()
 {
-    std::cout << "Adventofcode 2022, day 02\n";
+    std::cout << "AOC 2022, Day 2: Rock Paper Scissors \n";
 
-    std::cout << "Part 1: ???" << '\n';
-    std::cout << "Part 2: ???" << '\n';
+    auto input{read_lines("day02.txt")};
+
+    int score_part1{0};
+    int score_part2{0};
+
+    for (auto &line : input)
+    {
+        char opponent_move{ line[0]};
+        char second_column{ line[2]};
+
+        char my_move1 = second_column + 'A' - 'X';
+        score_part1 += calculate_score(opponent_move, my_move1);
+
+        char my_move2 = calculate_move(opponent_move, second_column);
+        score_part2 += calculate_score(opponent_move, my_move2);
+    }
+
+    std::cout << "Part 1: " << score_part1 << '\n';
+    std::cout << "Part 2: " << score_part2 << '\n';
     return 0;
 }
---
+++ adventofcode2022/day03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "utils.h"
+
+int main()
+{
+    std::cout << "AOC 2022, Day 3 \n";
+
+    int score_part1{0};
+    int score_part2{0};
+
+    std::cout << "Part 1: " << score_part1 << '\n';
+    std::cout << "Part 2: " << score_part2 << '\n';
+    return 0;
+}
